9adb4f0f8990bdc95a6d9023bd77d5df8ba4d69b
Darien Salazar <darien.axell.salazar@gmail.com>
2024-03-18 19:28:16 -0500

    Made some edits to the header of the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>Welcome, this is my portfolio...</h1>
+      <header className="pb-16" />
+      <section className="flex justify-around text-white px-14">
+        <div className="pt-20 basis-2/5">
+          <h1 className="text-5xl text-lime-300 ">Darien Salazar</h1>
+          <p className="text-lg">Full-stack software developer.</p>
+        </div>
+        <div className="basis-3/5">
+          <h2 className="text-xl pb-2">Hi. My name is Darien...</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
+            cum sociis natoque penatibus et magnis. Volutpat diam ut venenatis
+            tellus in metus. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Ultricies integer quis auctor elit sed vulputate
+            mi. Suspendisse in est ante in nibh mauris cursus mattis molestie.
+            In hac habitasse platea dictumst vestibulum rhoncus est pellentesque
+            elit. Consequat semper viverra nam libero. Aliquet risus feugiat in
+            ante. Scelerisque in dictum non consectetur a erat nam at lectus.
+            Hac habitasse platea dictumst quisque sagittis purus sit amet.
+            Posuere ac ut consequat semper. Donec ultrices tincidunt arcu non
+            sodales. Proin sagittis nisl rhoncus mattis rhoncus urna neque.
+          </p>
+        </div>
+      </section>
+      <section className="flex p-11 text-white">
+        <div className="basis-1/3 bg-slate-600 hover:bg-stone-500 p-7">
+          <h2>Education</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
+            cum sociis natoque penatibus et magnis. Volutpat diam ut venenatis
+            tellus in metus. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Ultricies integer quis auctor elit sed vulputate
+            mi. Suspendisse in est ante in nibh mauris cursus mattis molestie.
+          </p>
+        </div>
+        <div className="basis-1/3 bg-cyan-950  hover:bg-stone-500 p-7">
+          <h2>Experience</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
+            cum sociis natoque penatibus et magnis. Volutpat diam ut venenatis
+            tellus in metus. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Ultricies integer quis auctor elit sed vulputate
+            mi. Suspendisse in est ante in nibh mauris cursus mattis molestie.
+          </p>
+        </div>
+        <div className="basis-1/3 bg-violet-950 hover:bg-stone-500 p-7">
+          <h2>Story</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
+            cum sociis natoque penatibus et magnis. Volutpat diam ut venenatis
+            tellus in metus. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Ultricies integer quis auctor elit sed vulputate
+            mi. Suspendisse in est ante in nibh mauris cursus mattis molestie.
+          </p>
+        </div>
+      </section>
     </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        paragraph: ['"Familjen Grotesk"', "sans-serif"],
+        nameTitle: ['"Zen Tokyo Zoo"', "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+};
